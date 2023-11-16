9e46b4018e73d12cc96062b84390d5b988d2f2e5
vuductai2003 <vuhech@gmail.com>
2023-11-16 14:59:48 +0700

    16/11

---
+++ model/pdo.php
@@ -0,0 +1,14 @@
+<?php
+function pdo_get_connection(){
+    $dburl = "mysql:host=localhost;dbname=dam;charset=utf8";
+    $username = 'root';
+    $password = '';
+
+    $conn = new PDO($dburl, $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $conn;
+}
+
+
+
+?>
